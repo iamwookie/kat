75ffdd47fcb53ccae49500587e6f308f1327b1b4
Wookie <bq1214@gmail.com>
2022-01-05 14:26:02 +0500

    feat(modules): Excluded bots from join cache

--- src/modules/Security/joinLogger.js
+++ src/modules/Security/joinLogger.js
@@ -6,13 +6,13 @@ module.exports = {
         client.joins = new Discord.Collection();
 
         client.on('guildMemberAdd', member => {
-            let cache = client.joins.get(member.guild.id) || [];
+            if (member.user.bot) return;
 
+            let cache = client.joins.get(member.guild.id) || [];
             if (cache.length == 1000) cache.shift();
 
             if (!cache.includes(member.id)) cache.push(member.id);
 
-            console.log(cache)
             client.joins.set(member.guild.id, cache)
         });
     }
