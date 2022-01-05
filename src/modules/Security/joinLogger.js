22f50e95c099b656856e5f25d90422d7575954a6
Wookie <bq1214@gmail.com>
2022-01-05 14:11:32 +0500

    fix(modules): Fixed join cache for multiple guilds

--- src/modules/Security/joinLogger.js
+++ src/modules/Security/joinLogger.js
@@ -1,11 +1,19 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'Join Logger',
     run(client) {
-        client.joinCache = []
+        client.joins = new Discord.Collection();
 
         client.on('guildMemberAdd', member => {
-            if (client.joinCache == 1000) { client.joinCache.shift(); }
-            client.joinCache.push(member);
+            let cache = client.joins.get(member.guild.id) || [];
+
+            if (cache.length == 1000) cache.shift();
+
+            if (!cache.includes(member.id)) cache.push(member.id);
+
+            console.log(cache)
+            client.joins.set(member.guild.id, cache)
         });
     }
 }
