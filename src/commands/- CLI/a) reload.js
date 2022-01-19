5f40a2c068c9760a7637afc27fb4bc4ffc4560aa
Wookie <bq1214@gmail.com>
2022-01-19 13:37:30 +0500

    fix(commands): added await to commands as reload is now an async function

--- src/commands/- CLI/a) reload.js
+++ src/commands/- CLI/a) reload.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: 'reload',
     group: 'CLI',
-    run(client) {
+    async run(client) {
         if (!client.commander) return console.log('❌ Commander not found.\n');
 
-        client.commander.reload();
+        await client.commander.reload();
         console.log('✅ Commander reloaded.\n');
     }
 };
--- src/commands/a) Misc/d) reload.js
+++ src/commands/a) Misc/d) reload.js
@@ -4,10 +4,11 @@ module.exports = {
     description: 'Refresh Commander.',
     hidden: true,
     users: ['244662779745665026'],
-    run(client, msg) {
+    async run(client, msg) {
         if (!client.commander) return msg.reply('Commander not found!').catch(() => msg.channel.send('Commander not found!'));
 
-        client.commander.reload(msg);
+        await client.commander.reload(msg);
+        
         msg.reply('✅ Commander reloaded.').catch(() => msg.channel.send('✅ Commander reloaded.'));
     }
 };
