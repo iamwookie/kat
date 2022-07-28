c8f83fae7ae4c281dcb180d112a11f6d1a2046ff
Wookie <bq1214@gmail.com>
2022-07-28 16:17:49 +0500

    feat(commands): added more options to cli command access

--- src/commands/Global/-- CLI/e) access.js
+++ src/commands/Global/-- CLI/e) access.js
@@ -7,36 +7,72 @@ module.exports = {
 
     const argsArray = args.split(' ');
 
-    if (argsArray[0] == 'users') {
+    if (argsArray[0] == 'list') {
       const command = argsArray[1];
-      const users = argsArray.splice(2);
 
       if (!command) return console.log('❌ Invalid Arguments.'.yellow);
+
+      let data = await client.database.getAccess(command);
+
+      console.log(`----- Command: ${command} -----`.green);
+
+      if (!Object.keys(data).length) console.log('\nNo Data Found.'.yellow);
+
+      if (data.guilds) console.log('\n--> Guilds:'.yellow + `\n${data.guilds.join(', \n')}`);
+
+      if (data.users) console.log('\n--> Users:'.yellow + `\n${data.users.join(', \n')}`);
+
+      return console.log('\n------------------------'.green);
+    }
+
+    if (argsArray[0] == 'users') {
+      const action = argsArray[1];
+      const command = argsArray[2];
+      const users = argsArray.slice(3);
+
+      if (!command || !action) return console.log('❌ Invalid Arguments.'.yellow);
       if (!users.length) return console.log('❌ Invalid User IDs.'.yellow);
 
       let data = await client.database.getAccess(command);
-      data.users ? data.users.push(...users) : data.users = users;
 
-      await client.database.setAccess(command, data);
+      if (action == 'add') {
+        data.users ? data.users.push(...users) : data.users = users;
+        await client.database.setAccess(command, data);
+        return console.log(`✅ Added User(s) To Command (${command}): ${users.join(', ')}`.green);
+      }
 
-      return console.log(`✅ Added Users To Command (${command}): ${users.join(', ')}`.green);
+      if (action == 'remove') {
+        if (!data.users) return console.log('❌ No Users Found.'.yellow);
+        data.users = data.users.filter(user => !users.includes(user));
+        await client.database.setAccess(command, data);
+        return console.log(`✅ Removed User(s) From Command (${command}): ${users.join(', ')}`.green);
+      }
     }
 
     if (argsArray[0] == 'guilds') {
-      const command = argsArray[1];
-      const guilds = argsArray.splice(2);
+      const action = argsArray[1];
+      const command = argsArray[2];
+      const guilds = argsArray.splice(3);
 
-      if (!command) return console.log('❌ Invalid Arguments.'.yellow);
+      if (!command || !action) return console.log('❌ Invalid Arguments.'.yellow);
       if (!guilds.length) return console.log('❌ Invalid Guild IDs.'.yellow);
 
       let data = await client.database.getAccess(command);
-      data.guilds ? data.guilds.push(...guilds) : data.guilds = guilds;
 
-      await client.database.setAccess(command, data);
+      if (action == 'add') {
+        data.guilds ? data.guilds.push(...guilds) : data.guilds = guilds;
+        await client.database.setAccess(command, data);
+        return console.log(`✅ Added Guild(s) To Command (${command}): ${guilds.join(', ')}`.green);
+      }
 
-      return console.log(`✅ Added Guilds To Command (${command}): ${guilds.join(', ')}`.green);
+      if (action == 'remove') {
+        if (!data.guilds) return console.log('❌ No Guilds Found.'.yellow);
+        data.guilds = data.guilds.filter(guild => !guilds.includes(guild));
+        await client.database.setAccess(command, data);
+        return console.log(`✅ Removed Guild(s) From Command (${command}): ${guilds.join(', ')}`.green);
+      }
     }
 
-    console.log('❌ Invalid Arguments.'.yellow);
+    return console.log('❌ Invalid Arguments.'.yellow);
   }
 };
