396519732751741bc8c0ff5e3e81d9e9609c4bd0
Wookie <bq1214@gmail.com>
2022-08-15 19:05:13 +0500

    feat(server): changed user route to only display needed information

--- src/server/routes/endpoints/users.js
+++ src/server/routes/endpoints/users.js
@@ -7,9 +7,9 @@ module.exports = (client) => {
         try {
             const id = req.params.id;
             if (!id) return res.status(400).send('Bad Request');
-            const user = await client.users.fetch(id);
+            const user = await client.users.fetch(id, { force: true });
             if (!user) return res.status(404).send('Not Found');
-            return res.json(user);
+            return res.json(parseUser(user));
         } catch (err) {
             console.error('Requests [users] (ERROR) >> Error Getting users/:id'.red);
             console.error(err);
@@ -19,3 +19,15 @@ module.exports = (client) => {
 
     return router;
 };
+
+function parseUser(user) {
+    return {
+        id: user.id,
+        username: user.username,
+        discriminator: user.discriminator,
+        tag: user.tag,
+        avatarURL: user.avatarURL({ dynamic: true }),
+        bannerURL: user.bannerURL({ dynamic: true }),
+        accentHex: user.hexAccentColor,
+    };
+}
