c7f2ad687eafd3a1cd83a192a908128473cb0532
Wookie <bq1214@gmail.com>
2022-12-21 21:56:31 +0500

    fix(server): fixed auth

--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -4,7 +4,7 @@ const { createLog } = require('@server/utils/logs');
 
 exports.withAuth = client => {
     return (req, res, next) => {
-        if (!req.headers.authorization || req.headers.authorization != client) {
+        if (!req.headers.authorization || req.headers.authorization != process.env.CAT_API_KEY) {
             client.logger?.warn('Server >> Unauthorized Request Received'.red);
             createLog(req, 'Unauthorized', 'access');
 
