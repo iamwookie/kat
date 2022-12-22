489172e7a8b259978e490ab950b56ae165d0f696
Wookie <bq1214@gmail.com>
2022-12-22 23:55:42 +0500

    feat(server): updated asap ban logging

--- src/server/controllers/asap.js
+++ src/server/controllers/asap.js
@@ -87,8 +87,8 @@ exports.createStaff = client => {
             const body = req.body;
             if (!body) return res.status(400).send('Bad Request');
 
-            const { ban, banLength, banReason, adminUser, adminSid, banUser, banUserSid, avi } = body;
-            if (!adminUser || !adminSid || !banUser || !banUserSid || !avi) return res.status(400).send('Bad Request');
+            const { ban, banLength, banReason, adminUser, adminSid, banUser, banUserSid, banUserProfile, banUserAvatar } = body;
+            if (!adminUser || !adminSid || !banUser || !banUserSid || !banUserProfile || !banUserAvatar) return res.status(400).send('Bad Request');
 
             const channel = await client.channels.fetch(channels.staff);
             if (!channel) return res.status(500).send('Internal Server Error');
@@ -99,8 +99,8 @@ exports.createStaff = client => {
                 .setThumbnail(avi)
                 .setColor(ban ? '#ff0000' : '#00ff00')
                 .addFields([
-                    { name: 'Player', value: `[${banUser} (${banUserSid})](https://steamcommunity.com/profiles/${banUserSid})` },
-                    { name: 'Admin', value: `[${adminUser} (${adminSid})](https://steamcommunity.com/profiles/${adminSid})` }
+                    { name: 'Player', value: `[${banUser} (${banUserSid})](${banUserProfile})` },
+                    { name: 'Admin', value: `${adminUser} (${adminSid})` }
                 ]);
 
             if (ban) {
