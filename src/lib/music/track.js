1073c59ec95955fa3bdf27f3d4b9e5f363716760
Wookie <bq1214@gmail.com>
2023-01-03 19:15:45 +0500

    refactor(lib): refactored track

--- src/lib/music/track.js
+++ src/lib/music/track.js
@@ -1,7 +1,7 @@
 const DiscordVoice = require('@discordjs/voice');
-const Commander = require('@commander');
 
 const play = require('play-dl');
+
 const MusicEmbed = require('@utils/embeds/music');
 
 class Track {
