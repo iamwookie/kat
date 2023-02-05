1a6dbc80e133c5b5ca79a0dab7553f7752e63f4e
Wookie <bq1214@gmail.com>
2023-02-05 09:44:57 +0500

    feat(commands): added guild command unbanall

---
+++ src/commands/guild/311942245899173888/b) moderation/unbanall.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+const ActionEmbed = require('@utils/embeds/action');
+const ErrorEmbed = require('@utils/embeds/error');
+
+module.exports = {
+    name: 'unbanall',
+    group: 'Moderation',
+    description: 'Unban everyone from the Discord.',
+
+    // AUTHORIZATION
+    guilds: [],
+    users: [],
+
+    // SLASH
+    data() {
+        return (
+            new SlashCommandBuilder()
+                .setName(this.name)
+                .setDescription(this.description)
+                .setDMPermission(false)
+        );
+    },
+
+    async run(client, int) {
+        const bans = await int.guild.bans.fetch();
+
+        let count = 0
+        for (const [_, ban] of bans) {
+            try {
+                await int.guild.members.unban(ban.user);
+                count++;
+            } catch (err) {
+                client.logger?.error(err);
+            }
+        }
+
+        return int.editReply({ embeds: [new ActionEmbed('success', `Unbanned \`${count}\` users from the Discord!`, int.user)] });
+    }
+}; 
