ca9015090ab6b83652710b0f4fb8cdcca9b7fbe2
Wookie <bq1214@gmail.com>
2023-03-16 18:42:28 +0500

    Initial commit

--- src/utils/functions/parseDuration.js
+++
@@ -1,8 +0,0 @@
-function parseDuration(time) {
-    let hours = Math.floor(time / 3600);
-    let minutes = Math.floor(time / 60);
-    let seconds = time - minutes * 60;
-    return `${hours > 0 ? hours + ':' : ''}${minutes > 0 ? minutes + ':' : ''}${seconds}`;
-};
-
-module.exports = parseDuration;
