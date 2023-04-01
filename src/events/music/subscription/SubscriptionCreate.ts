ea9dd1d70f2b1c8a664504ec6791c89cc1f7d99e
Wookie <bq1214@gmail.com>
2023-04-02 03:40:32 +0500

    refactor: changed console log on event

--- src/events/music/subscription/SubscriptionCreate.ts
+++ src/events/music/subscription/SubscriptionCreate.ts
@@ -1,7 +1,5 @@
 import { Event, KATClient as Client, Commander, Subscription as MusicSubscription } from "@structures/index.js";
 
-import chalk from "chalk";
-
 export class SubscriptionCreate extends Event {
     constructor(client: Client, commander: Commander) {
         super(client, commander, "subscriptionCreate");
@@ -11,8 +9,8 @@ export class SubscriptionCreate extends Event {
         this.client.logger.info(`Music >> Subscription Created for ${subscription.guild.name} (${subscription.guild.id}). Node: ${subscription.node.name}`);
         setTimeout(() => {
             {
-                if (!subscription.active || !subscription.queue.length) subscription.destroy()
-                console.warn(chalk.yellowBright(`Music >> Subscription Destroyed (Inactivity) for ${subscription.guild.name} (${subscription.guild.id}).`))
+                if (!subscription.active || !subscription.queue.length) subscription.destroy();
+                this.client.logger.warn(`Music >> Subscription Destroyed (Inactivity) for ${subscription.guild.name} (${subscription.guild.id}).`);
             }
         }, 15_000);
     }
