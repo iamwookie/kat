fb31a8d5d033f6ec4183bb00c5362fd7a58c235b
Wookie <bq1214@gmail.com>
2023-04-05 20:27:52 +0500

    chore: update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -19,8 +19,6 @@ module.exports = {
 
                 TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
                 TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET,
-
-                GENIUS_API_KEY: process.env.GENIUS_API_KEY,
             },
         },
     ],
