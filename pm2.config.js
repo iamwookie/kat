2f56c57ec98231f04a1cce714c75bae06a288e50
iamwookie <iamwookie@users.noreply.github.com>
2023-04-12 19:07:39 +0000

    deploy: d0e1ac201c2b96a495e4849c24a79f2f5f49d43e

--- pm2.config.js
+++ pm2.config.js
@@ -6,20 +6,7 @@ module.exports = {
             instances: 1,
             autorestart: true,
             watch: "kat",
-            env: {
-                NODE_ENV: "production",
-
-                SENTRY_DSN: "https://2d9d272b4aaa4fcdb5b032a796cea564@o4504363480383488.ingest.sentry.io/4504363481956352",
-
-                BOT_TOKEN: process.env.BOT_TOKEN,
-                BOT_APP_ID: process.env.BOT_APP_ID,
-                KAT_API_KEY: process.env.CAT_API_KEY,
-
-                REDIS_URL: process.env.REDIS_URL,
-
-                TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
-                TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET,
-            },
+            env_file: ".env.production",
         },
     ],
 };
