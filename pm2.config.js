6a70d92fec14edf65f95bb552bca0f40f20d677e
Wookie <bq1214@gmail.com>
2023-04-14 22:00:23 +0500

    Revert "chore: update pm2.config.js"
    This reverts commit ccd7dcafeae2bdf89f726a8593700594fc9a2f7e.

--- pm2.config.js
+++ pm2.config.js
@@ -2,13 +2,24 @@ module.exports = {
     apps: [
         {
             name: "kat",
-            script: "kat/index.js",
+            script: "kat/dist/index.js",
             instances: 1,
             autorestart: true,
-            watch: "kat",
+            watch: "kat/dist",
             env: {
                 NODE_ENV: "production",
-            }
+
+                SENTRY_DSN: "https://2d9d272b4aaa4fcdb5b032a796cea564@o4504363480383488.ingest.sentry.io/4504363481956352",
+
+                BOT_TOKEN: process.env.BOT_TOKEN,
+                BOT_APP_ID: process.env.BOT_APP_ID,
+                KAT_API_KEY: process.env.CAT_API_KEY,
+
+                REDIS_URL: process.env.REDIS_URL,
+
+                TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+                TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET,
+            },
         },
     ],
 };
