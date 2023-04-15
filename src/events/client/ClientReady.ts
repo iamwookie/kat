c475a61e77e5fbe592825ac3b46ac2854f1493e0
Wookie <bq1214@gmail.com>
2023-04-15 05:42:55 +0500

    feat: better logging

--- src/events/client/ClientReady.ts
+++ src/events/client/ClientReady.ts
@@ -14,7 +14,7 @@ export class ClientReady extends Event {
         await this.client.server.initialize();
         console.log(chalk.greenBright.bold.underline(`>>> Server Initialized (Port: ${this.client.server.port})`));
 
-        console.log(chalk.magenta.bold.underline(`\n>>> App Online, Client: ${client.user?.tag} (${client.user?.id}) [Guilds: ${client.guilds.cache.size}]`));
-        console.log(chalk.magenta.bold.underline(`>>> App Loaded In: ${Date.now() - this.client.startTime}ms`));
+        console.log(chalk.magenta.bold.underline(`\n!! >>> App Online, Client: ${client.user?.tag} (${client.user?.id}) [Guilds: ${client.guilds.cache.size}]`));
+        console.log(chalk.magenta.bold.underline(`!! >>> App Loaded In: ${Date.now() - this.client.startTime}ms`));
     }
 }
