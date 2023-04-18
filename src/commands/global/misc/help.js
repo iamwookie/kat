da916694880f776d7dbc2a3f81d0699674fa86b4
iamwookie <iamwookie@users.noreply.github.com>
2023-04-18 16:12:33 +0000

    deploy: 2be0d68f2bf31449f5576180c4242a79c2f78bae

--- src/commands/global/misc/help.js
+++ src/commands/global/misc/help.js
@@ -18,7 +18,7 @@ export class HelpCommand extends Command {
     async execute(int) {
         const author = this.getAuthor(int);
         const res = await this.client.cache.getConfig(int.guild?.id);
-        const prefix = res?.prefix || this.client.prefix;
+        const prefix = res?.prefix || this.client.legacyPrefix;
         const replyEmbed = new EmbedBuilder()
             .setTitle("**Help Menu**")
             .setFooter({ text: "Parameters with a '?' at the start are optional." })
