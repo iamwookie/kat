dda77a3f7dcf7d57ac6ec073343e38800b91dd75
iamwookie <iamwookie@users.noreply.github.com>
2023-04-27 15:44:48 +0000

    deploy: 348412f937d081fd52ddb7e2dc5d36f3e45f149e

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,14 +1,24 @@
 import { YouTubeTrack, SpotifyTrack, YouTubePlaylist, SpotifyPlaylist } from '../structures/index.js';
 import { emotes } from '../../config.js';
 const musicEmotes = emotes.music;
-export function formatDuration(timeInMs) {
-    if (!timeInMs)
+export function formatDuration(milliseconds) {
+    if (!milliseconds)
         return 'No Data';
-    const time = Math.floor(timeInMs / 1000);
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor(time / 60);
-    const seconds = time - minutes * 60;
-    return `${hours > 0 ? hours.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + ':' : ''}${minutes > 0 ? minutes.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + ':' : ''}${seconds.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })}`;
+    const seconds = Math.floor(milliseconds / 1000);
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+    if (hours > 0) {
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${remainingSeconds
+            .toString()
+            .padStart(2, '0')}`;
+    }
+    else if (minutes > 0) {
+        return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+    }
+    else {
+        return `${remainingSeconds.toString().padStart(2, '0')}`;
+    }
 }
 export function formatBytes(bytes) {
     if (!bytes)
