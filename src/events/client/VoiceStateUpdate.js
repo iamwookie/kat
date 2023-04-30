a1c639912f5bbd6b628c20f63793be18eac644be
iamwookie <iamwookie@users.noreply.github.com>
2023-04-30 12:38:39 +0000

    deploy: e94d53ea0bdd6b7ea25ef3230add6f578ef5b4e8

--- src/events/client/VoiceStateUpdate.js
+++ src/events/client/VoiceStateUpdate.js
@@ -8,6 +8,8 @@ export class VoiceStateUpdate extends Event {
         const subcription = this.client.subscriptions.get(oldState.guild.id);
         if (!subcription)
             return;
+        if (oldState.id == this.client.user?.id && !newState.channel)
+            subcription.destroy();
         if (oldState.id == this.client.user?.id &&
             newState.id == this.client.user?.id &&
             oldState.channel &&
