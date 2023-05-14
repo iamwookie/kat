359aaa6e7dd92e0086f67edc1371d89a09156989
iamwookie <iamwookie@users.noreply.github.com>
2023-05-14 17:49:54 +0000

    deploy: 25cdd24a47fcf62509ea0c7e88aee8296e55164b

--- src/modules/affiliate.js
+++ src/modules/affiliate.js
@@ -12,7 +12,6 @@ export class AffiliateModule extends Module {
         this.channels.set('1023866029069320242', ['1096530697876930560']);
         this.channels.set('1094860861505544314', ['1094861185310011412']);
         this.on('ready', this.onReady.bind(this));
-        this.on('messageCreate', this.onMessageCreate.bind(this));
         this.on('inviteCreate', this.onInviteCreate.bind(this));
         this.on('guildCreate', this.onGuildCreate.bind(this));
         this.on('guildMemberAdd', this.onGuildMemberAdd.bind(this));
@@ -25,17 +24,6 @@ export class AffiliateModule extends Module {
         for (const guild of guilds.values()) {
             const invites = await guild.invites.fetch();
             this.invites.set(guild.id, new Collection(invites.map((invite) => [invite.code, invite.uses ?? 0])));
-        }
-    }
-    async onMessageCreate(message) {
-        const channels = ['1095042493197844651', '1023921941364604969'];
-        if (!message.channel || !channels.includes(message.id))
-            return;
-        try {
-            await message.channel.send('<@&1095034387613089822>');
-        }
-        catch (err) {
-            this.client.logger.error(err, 'Error Pinging Roles', `Module (${this.name})`);
         }
     }
     async onInviteCreate(invite) {
