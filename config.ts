cd51f75e9cd7ff20fec62b93385ad78876d6452c
Wookie <bq1214@gmail.com>
2023-06-24 23:10:45 +0500

    chore: update config.ts

--- config.ts
+++ config.ts
@@ -29,7 +29,7 @@ export const cache = {
     guildTimeout: 300, // The time in seconds when the guild cache will expire
     musicTimeout: 300, // The time in seconds when the music cache will expire
     queueTimeout: 300, // The time in seconds when the queue cache will expire
-}
+};
 
 export const server = {
     port: 3030,
@@ -47,8 +47,9 @@ export const lavalink = {
     nodes: [
         {
             name: 'uk:london-1',
-            url: process.env.LAVALINK_HOST,
-            auth: process.env.LAVALINK_AUTH,
+            url: 'lavalink.bil.al',
+            auth: 'yoruistrash',
+            secure: true,
         },
     ],
 };
