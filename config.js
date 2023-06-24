e9b2144d89926e6855a7c821d15c8185e968ee64
iamwookie <iamwookie@users.noreply.github.com>
2023-06-24 18:11:35 +0000

    deploy: 48a7554e742754a5917e6db349b21a35fc2cfa7d

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 import { PermissionsBitField } from 'discord.js';
-export const version = '7.11.2';
+export const version = '7.11.3';
 export const devs = ['244662779745665026'];
 export const bot = {
     prefix: process.env.NODE_ENV != 'production' ? '!' : '.',
