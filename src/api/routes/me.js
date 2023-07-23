01a37eb1eb204920ed32da619fc3179b043b2878
iamwookie <iamwookie@users.noreply.github.com>
2023-07-23 11:26:13 +0000

    deploy: 98164ea7e45e4e76bea5bf350d6fad3cf4711dfe

--- src/api/routes/me.js
+++ src/api/routes/me.js
@@ -1,4 +1,4 @@
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="5c80a101-99e6-5346-9b0b-5fd92290edd4")}catch(e){}}();
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="6e478cd6-936f-5baf-b393-67fab5b23a57")}catch(e){}}();
 import { Router } from 'express';
 import { withLimiter } from '../middlewares/limiter.middleware.js';
 import * as UserController from '../controllers/user.controller.js';
@@ -7,5 +7,5 @@ export default (client) => {
     router.get('/', withLimiter, UserController.fetchMe(client));
     return router;
 };
-//# debugId=5c80a101-99e6-5346-9b0b-5fd92290edd4
+//# debugId=6e478cd6-936f-5baf-b393-67fab5b23a57
 //# sourceMappingURL=me.js.map
